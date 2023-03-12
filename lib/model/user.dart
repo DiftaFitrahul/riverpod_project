fcf95f4ff51225a0f80e38d41716988e75f36644
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-03-12 22:34:28 +0700

    feat: finish create model for tutorial 3

---
+++ lib/model/user.dart
@@ -0,0 +1,11 @@
+class User {
+  final String name;
+  final String description;
+  final int age;
+
+  User({
+    required this.name,
+    required this.description,
+    required this.age,
+  });
+}
