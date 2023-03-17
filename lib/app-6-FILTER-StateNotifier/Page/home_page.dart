58acc247edfaf8a2433f15ab92a93429eee4bad1
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-03-18 00:12:18 +0700

    feat: creating homePage

--- lib/app-6-FILTER-StateNotifier/Page/home_page.dart
+++ lib/app-6-FILTER-StateNotifier/Page/home_page.dart
@@ -1,6 +1,29 @@
-class Page {
-  final String title;
-  final String description;
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_project/app-6-FILTER-StateNotifier/provider/drop_down_list.dart';
 
-  Page({required this.title, required this.description});
+class HomePageApp6 extends StatelessWidget {
+  const HomePageApp6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Consumer(
+            builder: (context, ref, child) {
+              final listDropDown = ref.watch(dropDownProvider);
+              return DropdownButton<String>(
+                value: listDropDown.first,
+                items: items,
+                onChanged: onChanged,
+              );
+            },
+          )
+        ],
+      )),
+    );
+  }
 }
---
+++ lib/app-6-FILTER-StateNotifier/model/page.dart
@@ -0,0 +1,6 @@
+class Page {
+  final String title;
+  final String description;
+
+  Page({required this.title, required this.description});
+}
