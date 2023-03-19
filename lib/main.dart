d1d325c9f3317355fa5e35ce761672542ac89200
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-03-19 20:30:11 +0700

    feat: initialize firebase in flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:riverpod_project/firebase_options.dart';
 
 import '../app-6-FILTER-StateNotifier/Page/home_page.dart';
 import '../CRUD_changenotifier/page/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
