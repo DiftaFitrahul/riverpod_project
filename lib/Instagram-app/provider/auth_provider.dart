7e637cd5a907bb6b20ddbb67463eb4e17a702244
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-03-22 16:44:52 +0700

    feat: finish create provider for facebook login

--- lib/Instagram-app/provider/auth_provider.dart
+++ lib/Instagram-app/provider/auth_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_project/Instagram-app/Service/google_facebook.dart';
 
@@ -22,3 +23,22 @@ class AuthGoogle extends StateNotifier<bool> {
 
 final googleSignInProvider =
     StateNotifierProvider<AuthGoogle, bool>((ref) => AuthGoogle(ref));
+
+class AuthFacebook extends StateNotifier<bool> {
+  AuthFacebook(this.ref) : super(false);
+  final Ref ref;
+
+  Future<void> signinFacebook() async {
+    try {
+      state = true;
+      await ref.read(authProvider).signInFacebook();
+      state = false;
+    } catch (e) {
+      state = false;
+      rethrow;
+    }
+  }
+}
+
+final facebookSignInProvider =
+    StateNotifierProvider<AuthFacebook, bool>((ref) => AuthFacebook(ref));
