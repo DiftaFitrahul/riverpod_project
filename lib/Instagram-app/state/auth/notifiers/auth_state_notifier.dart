205179bbde3ff9ad1d5df6941faa58af7febcc08
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-03-30 19:03:50 +0700

    feat: still working with loginWithGoogle

--- lib/Instagram-app/state/auth/notifiers/auth_state_notifier.dart
+++ lib/Instagram-app/state/auth/notifiers/auth_state_notifier.dart
@@ -4,11 +4,34 @@ import 'package:riverpod_project/Instagram-app/state/auth/models/auth_result.dar
 
 import '../models/auth_state.dart';
 
-class AuthStateNotifier extends StateNotifier<AuthState>{
+class AuthStateNotifier extends StateNotifier<AuthState> {
   final _authenticator = const Authenticator();
-  AuthStateNotifier(): super(const AuthState.unknown()){
-    if(_authenticator.isAlreadyLoggedIn){
-      state = AuthState(result: AuthResult.success, isLoading: false, userId: _authenticator.userId);
+  AuthStateNotifier() : super(const AuthState.unknown()) {
+    if (_authenticator.isAlreadyLoggedIn) {
+      state = AuthState(
+          result: AuthResult.success,
+          isLoading: false,
+          userId: _authenticator.userId);
+    }
+  }
+
+  Future<void> logOut() async {
+    state = state.copiedWithIsLoading(true);
+    await _authenticator.logOut();
+    state = const AuthState.unknown();
+  }
+
+  Future<void> logInWithGoogle() async {
+    state = state.copiedWithIsLoading(true);
+    final result = await _authenticator.loginWithGoole();
+    final userId = _authenticator.userId;
+    if(result == AuthResult.success && userId != null) {
+      state = AuthState(
+          result: result,
+          isLoading: false,
+          userId: userId);
+    } else {
+      state = state.copiedWithIsLoading(false);
     }
   }
 }
