edd40ed0b7dfca4415967bf7d32598d71355ccb1
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-04-05 22:25:06 +0700

    fix: comment to the provider

--- lib/Instagram-app/state/auth/providers/user_id_provider.dart
+++ lib/Instagram-app/state/auth/providers/user_id_provider.dart
@@ -3,5 +3,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../providers/auth_state_provider.dart';
 import '../../post/typedefs/user_id.dart';
 
+// create userIdProvider
 final userIdProvider =
     Provider<UserId?>((ref) => ref.watch(authStateNotifierProvider).userId);
