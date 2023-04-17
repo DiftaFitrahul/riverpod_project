1175096811ea5dd6369c02a024d904340f0e9bb0
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-04-17 15:47:28 +0700

    feat: create plaintext, and linktext factory

--- lib/Instagram-app/views/components/rich_text/base_text.dart
+++ lib/Instagram-app/views/components/rich_text/base_text.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/foundation.dart' show immutable;
+import 'package:flutter/foundation.dart' show VoidCallback, immutable;
 import 'package:flutter/material.dart' show TextStyle, Colors, TextDecoration;
+import 'package:riverpod_project/Instagram-app/views/components/rich_text/link_text.dart';
 
 @immutable
 class BaseText {
@@ -8,8 +9,15 @@ class BaseText {
 
   const BaseText({required this.text, this.style});
 
-  factory BaseText.plainText({
+  factory BaseText.plainText(
+          {required String text, TextStyle? style = const TextStyle()}) =>
+      BaseText(text: text, style: style);
+
+  factory BaseText.linkText({
     required String text,
-    TextStyle? style = const TextStyle()
-  }) => BaseText(text: text, style: style);
+    required VoidCallback callback,
+    TextStyle? style = const TextStyle(
+        color: Colors.blue, decoration: TextDecoration.underline),
+  }) =>
+      LinkText(text: text, callback: callback, style: style);
 }
