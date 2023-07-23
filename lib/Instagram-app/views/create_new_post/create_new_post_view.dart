87dc506d897004bc1934c3155cb5d3d33e191a02
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2023-07-23 19:57:34 +0700

    add current update

--- lib/Instagram-app/views/create_new_post/create_new_post_view.dart
+++ lib/Instagram-app/views/create_new_post/create_new_post_view.dart
@@ -1,16 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_project/Instagram-app/state/image_upload/models/file_types.dart';
+import 'package:riverpod_project/Instagram-app/state/image_upload/models/thumbnail_request.dart';
 
 class CreateNewPostView extends StatefulHookConsumerWidget {
-  const CreateNewPostView({super.key});
+  final File fileToPost;
+  final FileType fileType;
+  const CreateNewPostView(
+      {super.key, required this.fileToPost, required this.fileType});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CreateNewPostViewState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _CreateNewPostViewState();
 }
 
 class _CreateNewPostViewState extends ConsumerState<CreateNewPostView> {
   @override
   Widget build(BuildContext context) {
+    final thumbnailRequest =
+        ThumbnailRequest(file: widget.fileToPost, fileType: widget.fileType);
     return const Placeholder();
   }
 }
